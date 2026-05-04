bdfc1580b168029d8cbc08805551e3a08f47d86b
AlephNLL <117766085+AlephNLL@users.noreply.github.com>
2026-05-04 03:06:42 +0100

    Hand point at selected object on the shop

--- Assets/_Scripts/Misc/ShopManager.cs
+++ Assets/_Scripts/Misc/ShopManager.cs
@@ -1,19 +1,23 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
     [SerializeField] GameObject[] slots;
     [SerializeField] Item[] itemPool;
+    [SerializeField] List<Item> items;
 
-    Item selectedItem;
+    public Item selectedItem;
+
+    int lastIndex = 0;
     private void Start()
     {
         HandAnimatorHelper.onAnimationEnd += OpenShop;
     }
     private void Update()
     {
-        
+        UpdateSelectedItem();
     }
     public void OpenShop()
     {
@@ -36,6 +40,55 @@ public class ShopManager : MonoBehaviour
         {
             Item item = itemPool[Random.Range(0, itemPool.Length)];
             Instantiate(item.icon, slots[i].transform);
+            items.Add(item);
         }
+    }
+
+    int GetItemSlotIndex(GameObject item)
+    {
+        int index = 0;
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (item.transform.IsChildOf(slots[i].transform))
+            {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    void UpdateSelectedItem()
+    {
+        GameObject itemUnderMouse = GraphicRaycasting.instance.GetObjectUnderMouse();
+
+        if (itemUnderMouse != null) 
+        {
+            StopAllCoroutines();
+            int itemSlotIndex = GetItemSlotIndex(itemUnderMouse);
+            float handXPos = itemSlotIndex - 3.75f;
+            if (itemSlotIndex >= 3) handXPos += 2f;
+            if (!HandAnimatorHelper.instance.IsHandAtXPos(handXPos))
+            {
+                selectedItem = items[itemSlotIndex];
+                HandAnimatorHelper.instance.MoveToPosition(new Vector3(handXPos, 4, HandAnimatorHelper.instance.transform.position.z));
+                HandAnimatorHelper.instance.SetHandBoolParameter("point", true);
+                lastIndex = itemSlotIndex;
+            }
+        }
+        else
+        {
+            selectedItem = null;
+            if (!HandAnimatorHelper.instance.IsHandAtXPos(0)) StartCoroutine(ReturnHandToDefautlPos());
+        }
+    }
+
+    IEnumerator ReturnHandToDefautlPos()
+    {
+        yield return new WaitForSeconds(.2f);
+        HandAnimatorHelper.instance.MoveToDefaultPosition();
+        HandAnimatorHelper.instance.SetHandBoolParameter("point", false);
     }
 }

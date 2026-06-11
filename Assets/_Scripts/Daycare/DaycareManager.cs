fe69c7c6ee56088c6e483ae5a8a00aaee65831b4
AlephNLL <117766085+AlephNLL@users.noreply.github.com>
2026-06-11 18:32:46 +0100

    summary fix

--- Assets/_Scripts/Daycare/DaycareManager.cs
+++ Assets/_Scripts/Daycare/DaycareManager.cs
@@ -5,8 +5,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
